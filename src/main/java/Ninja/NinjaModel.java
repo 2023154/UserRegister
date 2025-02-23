dd964201832ff511bd68d8e3b3ab174e3ec6f51f
2023154 <2023154@student.cct.ie>
2025-02-23 22:51:33 +0000

    fix: I am adding a @column in my class

--- src/main/java/Ninja/NinjaModel.java
+++ src/main/java/Ninja/NinjaModel.java
@@ -9,72 +9,27 @@ import mission.MissionModel;
 public class NinjaModel {
 
 
-
-
     @Id// the id will be gerated altomaticly with the generate value
     @GeneratedValue(strategy = GenerationType.IDENTITY)//this is going to generate a id value number.
 
-
+    @Column(name = "id_ninja")
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
-    @Column (unique = true)
+    @Column(unique = true)
     private String email;
 
-    private String age;
+    @Column(name = "img_url")
+    private String imgUrl;
 
+    @Column(name = "age")
+    private String age;
 
 
     //@ a ninja has only one mission
     @ManyToOne
     @JoinColumn(name = "missons_id")
     private MissionModel misson;
-
-
-
-    public NinjaModel(String name, String email, String age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-
-    public NinjaModel() {
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-
-
-    }
-    public void setEmail(String email) {
-        this.email = email;
-
-    }
-    public void setAge(String age) {
-        this.age = age;
-
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
-
